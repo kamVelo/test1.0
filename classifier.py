1f98a508bc207c93963208a8999b031e00f18016
Ali Kamel <alikamel2004@hotmail.com>
2020-08-17 04:40:17 +0100

    basic, inital attempt at k-means clustering

--- classifier.py
+++ classifier.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from matplotlib import  pyplot
 symbol = "aapl"
 dset = pd.read_csv(os.path.join(symbol, "dset.csv")).iloc[:, 1:].iloc[::-1]
 rsi  = pd.read_csv(os.path.join(symbol, "rsi.csv")).iloc[:, 1:].iloc[::-1]
@@ -17,5 +19,18 @@ sc_x = StandardScaler()
 rsi = sc_y.fit_transform(rsi)
 closes = dset["close"]
 closes = sc_x.fit_transform(closes.values.reshape(-1,1))
-plt.scatter(closes, rsi)
-plt.show()
+x= pd.DataFrame(rsi,columns=["rsi"])
+x["closes"] = closes;
+
+model = KMeans(
+    init="random",
+    n_clusters=5,
+    n_init= 3,
+    max_iter=300,
+    random_state=42
+)
+
+model.fit(x)
+print("SSE: %s" % model.inertia_)
+print("Number of clusters required to converge: %s" % model.n_iter_)
+
